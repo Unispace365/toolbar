aab67d71f04a071fe040b313eab4166da6e1d060
Matt Vogel <>
2024-01-30 13:49:12 -0500

    fix: unneeded install variables

--- install/Toolbar Install.bat
+++ install/Toolbar Install.bat
@@ -8,12 +8,10 @@ set command_name=extend
 set extension_type=ui
 set extension_name=Unispace
 set repository_url=https://github.com/Unispace365/revit-unispace-toolbar.git
-set username=SET_GITHUB_USERNAME_HERE
-set token=SET_GITHUB_TOKEN_HERE
 set branch=main
 
 REM Build the command
-set pyrevit_command=pyrevit %command_name% %extension_type% %extension_name% "%repository_url%" --username="%username%" --token="%token%" --branch="%branch%"
+set pyrevit_command=pyrevit %command_name% %extension_type% %extension_name% "%repository_url%" --branch="%branch%"
 
 REM Print the command to the console
 echo Running command: %pyrevit_command%
