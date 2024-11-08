f3d16aad5ab34810a6a5d2a577635d1c37a36007
Matt Vogel <matt.vogel@unispace.com>
2024-11-08 13:14:56 -0500

    get all params

--- Unispace.Tab/Working.Panel/SelectionToViewFilter.pushbutton/script.py
+++ Unispace.Tab/Working.Panel/SelectionToViewFilter.pushbutton/script.py
@@ -9,44 +9,78 @@ clr.AddReference('RevitAPIUI')
 from Autodesk.Revit.DB import *
 from Autodesk.Revit.UI import TaskDialog, TaskDialogCommonButtons, TaskDialogResult
 from pyrevit import revit, DB
+from System.Collections.Generic import List
 
 # Set the active Revit application and document
 doc = __revit__.ActiveUIDocument.Document
 uidoc = __revit__.ActiveUIDocument
 
+def get_parameter_value(param):
+    """Get parameter value based on storage type"""
+    if not param:
+        return "Parameter not found"
+    
+    try:
+        if param.StorageType == StorageType.String:
+            return param.AsString() or "No Value"
+        elif param.StorageType == StorageType.Integer:
+            return str(param.AsInteger())
+        elif param.StorageType == StorageType.Double:
+            return str(param.AsDouble())
+        elif param.StorageType == StorageType.ElementId:
+            return str(param.AsElementId().IntegerValue)
+        else:
+            return param.AsValueString() or "No Value"
+    except:
+        return "Error getting value"
+
+def get_built_in_param_name(param):
+    """Try to get built-in parameter name if it exists"""
+    try:
+        if param.Definition.BuiltInParameter != BuiltInParameter.INVALID:
+            return str(param.Definition.BuiltInParameter)
+    except:
+        pass
+    return "Not built-in"
+
 def get_parameter_info(param):
     """Get detailed information about a parameter"""
     try:
-        # Create dictionary with parameter information
         param_info = dict(
             name = param.Definition.Name,
+            built_in_name = get_built_in_param_name(param),
             category = str(param.Definition.ParameterGroup),
             parameter_type = str(param.Definition.ParameterType),
             storage_type = str(param.StorageType),
             is_shared = param.IsShared,
             guid = str(param.GUID) if param.IsShared else "Not a shared parameter",
             element_id = param.Id.IntegerValue,
-            value = param.AsString() or param.AsValueString() or "No Value"
+            value = get_parameter_value(param)
         )
         return param_info
     except Exception as e:
         return "Error getting parameter info: {0}".format(str(e))
 
-def get_parameter_value(param):
-    """Get parameter value based on storage type"""
-    if not param:
-        return "No Parameter"
+def get_all_parameters(element):
+    """Get all parameters from both instance and type"""
+    param_data = []
     
-    if param.StorageType == StorageType.String:
-        return param.AsString()
-    elif param.StorageType == StorageType.Integer:
-        return param.AsInteger()
-    elif param.StorageType == StorageType.Double:
-        return param.AsDouble()
-    elif param.StorageType == StorageType.ElementId:
-        return param.AsElementId().IntegerValue
-    else:
-        return param.AsValueString()
+    # Get instance parameters
+    for param in element.Parameters:
+        param_info = get_parameter_info(param)
+        if isinstance(param_info, dict):
+            param_data.append(param_info)
+    
+    # Get type parameters
+    element_type = doc.GetElement(element.GetTypeId())
+    if element_type:
+        for param in element_type.Parameters:
+            param_info = get_parameter_info(param)
+            if isinstance(param_info, dict):
+                param_info['name'] = "Type: " + param_info['name']  # Prefix type parameters
+                param_data.append(param_info)
+    
+    return param_data
 
 def display_element_info(element):
     """Display comprehensive information about the element"""
@@ -57,28 +91,33 @@ def display_element_info(element):
         "Category: {0}".format(element.Category.Name),
         "Element ID: {0}".format(element.Id.IntegerValue),
         "UniqueId: {0}".format(element.UniqueId),
-        "\nParameters:",
+        "Family Name: {0}".format(element.Symbol.Family.Name if hasattr(element, 'Symbol') else "N/A"),
+        "Type Name: {0}".format(element.Name),
+        "\nAll Available Parameters:",
         "-" * 50
     ]
     
     # Get all parameters
-    for param in element.Parameters:
-        param_info = get_parameter_info(param)
-        if isinstance(param_info, dict):
-            param_strings = [
-                "\nParameter: {0}".format(param_info['name']),
-                "Category: {0}".format(param_info['category']),
-                "Parameter Type: {0}".format(param_info['parameter_type']),
-                "Storage Type: {0}".format(param_info['storage_type']),
-                "Is Shared: {0}".format(param_info['is_shared']),
-                "GUID: {0}".format(param_info['guid']),
-                "Element ID: {0}".format(param_info['element_id']),
-                "Value: {0}".format(param_info['value']),
-                "-" * 50
-            ]
-            info_strings.extend(param_strings)
-        else:
-            info_strings.append(param_info)
+    parameters = get_all_parameters(element)
+    
+    # Sort parameters by name for better readability
+    parameters.sort(key=lambda x: x['name'])
+    
+    # Add parameter information to display strings
+    for param_info in parameters:
+        param_strings = [
+            "\nParameter: {0}".format(param_info['name']),
+            "Built-in Parameter: {0}".format(param_info['built_in_name']),
+            "Category: {0}".format(param_info['category']),
+            "Parameter Type: {0}".format(param_info['parameter_type']),
+            "Storage Type: {0}".format(param_info['storage_type']),
+            "Is Shared: {0}".format(param_info['is_shared']),
+            "GUID: {0}".format(param_info['guid']),
+            "Element ID: {0}".format(param_info['element_id']),
+            "Value: {0}".format(param_info['value']),
+            "-" * 50
+        ]
+        info_strings.extend(param_strings)
     
     # Display the information in a TaskDialog
     dialog = TaskDialog("Element Information")
